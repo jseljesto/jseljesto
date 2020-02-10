43ea1b35e51818e7db23232cb301e80d852ef00b
Jon Kristian Seljestø <jse009@student.uib.no>
2020-02-10 14:50:48 +0100

    Data now parsed to html template

--- Quiz/quiz.js
+++ Quiz/quiz.js
@@ -1,11 +1,11 @@
 let fs = require('fs');
 let retrievedData = fs.readFileSync('./data.json');
 let parsedData = JSON.parse(retrievedData);
-console.log(parsedData);
 
 const express = require('express');
 const app = express();
 const path = require('path');
+//const questionMethods = require('./views/questionFunc.js');
 
 const port = 8080;
 
@@ -32,7 +32,7 @@ answers.sort(function (a, b) {
 });
 
 app.get('/', function (req, res) {
-    res.render('quiz', {
+    res.render('index', {
         questionsheet: parsedData[qAnswered].question,
         option1: answers[0],
         radio1: answers[0],
@@ -45,63 +45,9 @@ app.get('/', function (req, res) {
     });
 });
 
-function checkQuestion() {
-    for (i = 1; i < 5; i++) {
-        if (document.getElementById("radio" + i).checked) {
-            if (document.getElementById("radio" + i).value ===
-                parsedData[qAnswered].answers.correctAnswer) {
-                alert("You answered correctly!");
-                points++;
-                document.getElementById("option" + i).style.color = "green";
-            }
-            else {
-                alert("You answered wrong!");
-                document.getElementById("option" + i).style.color = "red";
-            }
-        }
-    }
-    qAnswered++;
-    document.getElementById("check").style.visibility = "hidden";
-    document.getElementById("next").style.visibility = "visible";
-}
+//questionMethods.checkQuestion();
+//questionMethods.nextQuestion();
 
 app.listen(port, function() {
     console.log(`Server started at port ${port}`);
 });
-
-
-/*
-function nextQuestion() {
-
-    if (qAnswered === numOfQuestions) {
-        document.getElementById("next").style.visibility = "visible";
-        alert("You are done!");
-    }
-    else {
-
-        answers = [parsedData[qAnswered].answers.correctAnswer, parsedData[qAnswered].answers.wrongAnswer1,
-        parsedData[qAnswered].answers.wrongAnswer2, parsedData[qAnswered].answers.wrongAnswer3];
-
-        answers.sort(function (a, b) {
-            return 0.5 - Math.random()
-        });
-
-        document.getElementById("questionsheet").innerHTML = parsedData[qAnswered].question;
-        document.getElementById("option1").innerHTML = answers[0];
-        document.getElementById("radio1").value = answers[0];
-        document.getElementById("option2").innerHTML = answers[1];
-        document.getElementById("radio2").value = answers[1];
-        document.getElementById("option3").innerHTML = answers[2];
-        document.getElementById("radio3").value = answers[2];
-        document.getElementById("option4").innerHTML = answers[3];
-        document.getElementById("radio4").value = answers[3];
-
-        document.getElementById("option1").style.color = "black";
-        document.getElementById("option2").style.color = "black";
-        document.getElementById("option3").style.color = "black";
-        document.getElementById("option4").style.color = "black";
-
-        document.getElementById("check").style.visibility = "visible";
-        document.getElementById("next").style.visibility = "hidden";
-    }
-} */
