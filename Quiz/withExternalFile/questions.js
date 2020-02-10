af661a30d001e416b9aa42cff9e0c2a8c41eea28
Jon Kristian Seljestø <jse009@student.uib.no>
2020-02-10 15:31:14 +0100

    Added temporary functioning version

---
+++ Quiz/withExternalFile/questions.js
@@ -0,0 +1,29 @@
+export let data = [
+    {
+       "question": "What colours are represented on the Norwegian flag?",
+       "answers": {
+          "correctAnswer": "Red, blue and white",
+          "wrongAnswer1": "Yellow, blue and white",
+          "wrongAnswer2": "Red and white",
+          "wrongAnswer3": "Blue and white"
+       }
+    },
+    {
+       "question": "What is the national day of Norway?",
+       "answers": {
+          "correctAnswer": "17th of May",
+          "wrongAnswer1": "20th of May",
+          "wrongAnswer2": "17th of April",
+          "wrongAnswer3": "20th of April"
+       }
+    },
+    {
+       "question": "What is the population of Norway?",
+       "answers": {
+          "correctAnswer": "5,3 million",
+          "wrongAnswer1": "4,3 million",
+          "wrongAnswer2": "4,8 million",
+          "wrongAnswer3": "5.8 million"
+       }
+    }
+ ]
