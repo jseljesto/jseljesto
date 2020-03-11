924281136efc9d784bc79bb03d85f13f5f6240a6
jseljesto <jonkristian96@hotmail.com>
2020-03-11 21:07:09 +0100

    Implemented random waves of monsters

---
+++ Minigames/monsters.js
@@ -0,0 +1,38 @@
+export let monsters = [
+    {
+        name: "Giant",
+        strength: 50
+    },
+    {
+        name: "Wolf",
+        strength: 10
+    },
+    {
+        name: "Black Rat",
+        strength: 2
+    },
+    {
+        name: "Orc",
+        strength: 20
+    },
+    {
+        name: "Zombie",
+        strength: 7
+    },
+    {
+        name: "Skeleton",
+        strength: 5
+    },
+    {
+        name: "Troll",
+        strength: 25
+    },
+    {
+        name: "Goblin",
+        strength: 3
+    },
+    {
+        name: "Bear",
+        strength: 15
+    }
+];
