9b24993c4dbb04a1e07daa26201bceb056c0eecc
Jon Kristian Seljestø <jse009@student.uib.no>
2020-08-05 20:25:49 +0200

    Creates new galaxy every page load

---
+++ src/solarSystem/planetGradients.js
@@ -0,0 +1,9 @@
+export let gradients = [
+    {firstColor:"black", secondColor:"gray", thirdColor:"white", fourthColor:"gray", fifthColor:"black"},
+    {firstColor:"purple", secondColor:"indigo", thirdColor:"white", fourthColor:"indigo", fifthColor:"purple"},
+    {firstColor:"navy", secondColor: "cyan", thirdColor:"green", fourthColor:"cyan", fifthColor:"navy"},
+    {firstColor:"red", secondColor:"purple", thirdColor:"navy", fourthColor:"purple", fifthColor: "red"},
+    {firstColor:"darkgreen", secondColor:"springgreen", thirdColor:"yellow", fourthColor:"springgreen", fifthColor:"darkgreen"},
+    {firstColor:"black", secondColor:"maroon", thirdColor:"red", fourthColor:"maroon", fifthColor:"black"},
+    {firstColor:"blue", secondColor:"navy", thirdColor:"black", fourthColor: "navy", fifthColor: "blue"}
+];
