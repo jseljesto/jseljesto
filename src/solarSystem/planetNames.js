835b83c7baa7807868470891ad8b1cfadda22490
Jon Kristian Seljestø <jonkristian96@hotmail.com>
2020-08-29 19:40:43 +0200

    Planets now include randomized names

---
+++ src/solarSystem/planetNames.js
@@ -0,0 +1,7 @@
+export let names = ["Sunothea", "Zibocury", "Valnion", "Sogiea", "Tulara", "Cruviphus", "Bronehiri", "Thichi 4C", "Sarvis S5W", "Bagucorro", "Kemozuno", "Giliri", "Pabrars", 
+                    "Camia", "Keyama", "Cuyobos", "Mailea", "Meon 4MKF", "Vinda O9", "Abritov", "Hicuater", "Chilrides", "Silvuna", "Meobos", "Tuatune", "Gaugawa", "Legistea",
+                    "Cradus OGZQ", "Lloth 3SE", "Suluitune", "Pongaicury", "Tacriri", "Damapus", "Routis", "Bestea", "Llegohiri", "Nianerth", "Brilles DG", "Trilia YQL", "Odrathea",
+                    "Tignuter", "Thasosie", "Ranerth", "Pubapus", "Tolia", "Gneenov", "Chabithea", "Strade 4V7", "Dromia HU3", "Nuvevis", "Halvaphus", "Thostreron", "Zephippe",
+                    "Newei", "Calea", "Trikutov", "Strakuwei", "Crorth 89D9", "Madus X0L", "Velruiclite", "Sanruzono", "Punorix", "Hongov", "Chorolia", "Neanope", "Sauruta",
+                    "Ludonus", "Dara VL", "Meshan 36L2"
+]
