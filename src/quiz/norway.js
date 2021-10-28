2c685bd2f7680d74e74afbb929cf31753c0b0404
Jon Kristian Seljestø <jonkristian96@hotmail.com>
2021-10-28 22:00:01 +0200

    Refoundation of quiz program including new buttons

---
+++ src/quiz/norway.js
@@ -0,0 +1,137 @@
+let questionArray = [
+    {
+       "question": "What colours are represented on the Norwegian flag?",
+       "answers": {
+          "correctAnswer": "Red, blue and white",
+          "wrongAnswer1": "Yellow, blue and white",
+          "wrongAnswer2": "Red and white",
+          "wrongAnswer3": "Blue and white"
+       }
+    },
+    {
+       "question": "What is the national day of Norway?",
+       "answers": {
+          "correctAnswer": "17th of May",
+          "wrongAnswer1": "20th of May",
+          "wrongAnswer2": "17th of April",
+          "wrongAnswer3": "20th of April"
+       }
+    },
+    {
+       "question": "What is the population of Norway?",
+       "answers": {
+          "correctAnswer": "5,3 million",
+          "wrongAnswer1": "4,3 million",
+          "wrongAnswer2": "4,8 million",
+          "wrongAnswer3": "5,8 million"
+       }
+    },
+    {
+      "question": "What is the capital of Norway?",
+      "answers": {
+         "correctAnswer": "Oslo",
+         "wrongAnswer1": "Helsinki",
+         "wrongAnswer2": "Stockholm",
+         "wrongAnswer3": "København"
+      }
+   },
+   {
+      "question": "What is the currency of Norway?",
+      "answers": {
+         "correctAnswer": "Krone",
+         "wrongAnswer1": "Euro",
+         "wrongAnswer2": "Nordikyn",
+         "wrongAnswer3": "Shilling"
+      }
+   },
+   {
+      "question": "Which of these organizations are Norway not a part of?",
+      "answers": {
+         "correctAnswer": "The European Union (EU)",
+         "wrongAnswer1": "World Trade Organization (WTO)",
+         "wrongAnswer2": "United Nations (UN)",
+         "wrongAnswer3": "North Atlantic Treaty Organization (NATO)"
+      }
+   },
+   {
+      "question": "What is the name of the current King of Norway?",
+      "answers": {
+         "correctAnswer": "King Harald V",
+         "wrongAnswer1": "King Olav III",
+         "wrongAnswer2": "King Ludwig II",
+         "wrongAnswer3": "King Edvard X"
+      }
+   },
+   {
+      "question": "On June 7, 1905, Norway became officially independent from which country?",
+      "answers": {
+         "correctAnswer": "Sweden",
+         "wrongAnswer1": "Great Britain",
+         "wrongAnswer2": "Denmark",
+         "wrongAnswer3": "Germany"
+      }
+   },
+   {
+      "question": "Which side did Norway officially support during World War II?",
+      "answers": {
+         "correctAnswer": "Neutral",
+         "wrongAnswer1": "Allied powers",
+         "wrongAnswer2": "Axis powers",
+         "wrongAnswer3": "United States of America"
+      }
+   },
+   {
+      "question": "The indigenous people of Norway are known as…",
+      "answers": {
+         "correctAnswer": "Sami",
+         "wrongAnswer1": "Maya",
+         "wrongAnswer2": "Tuareg",
+         "wrongAnswer3": "Tatars"
+      }
+   },
+   {
+      "question": "Which Nobel Prize is awareded in Norway each year?",
+      "answers": {
+         "correctAnswer": "Peace",
+         "wrongAnswer1": "Literature",
+         "wrongAnswer2": "Chemistry",
+         "wrongAnswer3": "Medicine"
+      }
+   },
+   {
+      "question": "What is the unofficial national dish of Norway?",
+      "answers": {
+         "correctAnswer": "Grandiosa frozen pizza",
+         "wrongAnswer1": "Taco",
+         "wrongAnswer2": "Meatballs",
+         "wrongAnswer3": "Jose Ole burritos"
+      }
+   },
+   {
+      "question": "Every year since 1947, Norway has presented England with…",
+      "answers": {
+         "correctAnswer": "A Christmas tree for Trafalgar Square",
+         "wrongAnswer1": "A pair of wooden shoes for the British Museum",
+         "wrongAnswer2": "An olive branch for the Prime Minister’s office",
+         "wrongAnswer3": "An honorary Norwegian passport for a British citizen"
+      }
+   },
+   {
+      "question": "Which countries does Norway border?",
+      "answers": {
+         "correctAnswer": "With Sweden, Finland and Russia",
+         "wrongAnswer1": "With Sweden and Finland",
+         "wrongAnswer2": "Only Sweden",
+         "wrongAnswer3": "With Sweden, Denmark and Finland"
+      }
+   },
+   {
+      "question": "Which famous painting was drawn by norwegian artist Edward Munch?",
+      "answers": {
+         "correctAnswer": "The Scream",
+         "wrongAnswer1": "The Last Supper",
+         "wrongAnswer2": "Girl With a Pearl Earring",
+         "wrongAnswer3": "The Night Watch"
+      }
+   },
+ ]
---
+++ src/quiz/topics.js
@@ -0,0 +1,56 @@
+let arts = [{name: "Theater", color: "", textColor:"", icon: ""},
+{name: "Paintings", color: "", textColor:"", icon: ""}];
+
+let business = [{name: "Currencies", color: "", textColor:"", icon: ""}];
+
+let educational = [{name: "Basic Math", color: "", textColor:"", icon: ""}];
+
+let entertainment = [{name: "World News", color: "", textColor:"", icon: ""},
+{name: "Futurama", color: "", textColor:"", icon: ""},
+{name: "Friends", color: "", textColor:"", icon: ""},
+{name: "Game of Thrones", color: "", textColor:"", icon: ""}];
+
+let games = [{name: "Game characters", color: "", textColor:"", icon: ""},
+{name: "GTA V", color: "", textColor:"", icon: ""},
+{name: "Pokemon", color: "", textColor:"", icon: ""},
+{name: "Tabletop games", color: "", textColor:"", icon: ""}];
+
+let geography = [{name: "Norway", color: "", textColor:"", icon: ""},
+{name: "US States", color: "", textColor:"", icon: ""}];
+
+let history = [{name: "Cold War", color: "", textColor:"", icon: ""}];
+
+let lifestyle = [{name: "Food", color: "", textColor:"", icon: ""},
+{name: "Web Culture", color: "", textColor:"", icon: ""},
+{name: "Religions", color: "", textColor:"", icon: ""}];
+
+let literature = [{name: "Lords of the Rings", color: "", textColor:"", icon: ""}];
+
+let movies = [{name: "Comedy", color: "", textColor:"", icon: ""}];
+
+let music = [{name: "Hip Hop", color: "", textColor:"", icon: ""},
+{name: "Rock", color: "", textColor:"", icon: ""},
+{name: "Pop", color: "", textColor:"", icon: ""},
+{name: "2010s", color: "", textColor:"", icon: ""},
+{name: "2000s", color: "", textColor:"", icon: ""},
+{name: "1990s", color: "", textColor:"", icon: ""}];
+
+let nature = [{name: "Fruits", color: "", textColor:"", icon: ""},
+{name: "Geology", color: "", textColor:"", icon: ""},
+{name: "Dogs", color: "", textColor:"", icon: ""},
+{name: "Cats", color: "", textColor:"", icon: ""}];
+
+let science = [{name: "Physics", color: "", textColor:"", icon: ""},
+{name: "Chemistry", color: "", textColor:"", icon: ""},
+{name: "Biology", color: "", textColor:"", icon: ""},
+{name: "Astronomy", color: "", textColor:"", icon: ""},
+{name: "Android", color: "", textColor:"", icon: ""},
+{name: "Microsoft", color: "", textColor:"", icon: ""},
+{name: "Medical", color: "", textColor:"", icon: ""},
+{name: "Meteorology", color: "", textColor:"", icon: ""},
+{name: "Elements", color: "", textColor:"", icon: ""}];
+
+let sports = [{name: "Football", color: "", textColor:"", icon: ""},
+{name: "Basketball", color: "", textColor:"", icon: ""},
+{name: "Baseball", color: "", textColor:"", icon: ""},
+{name: "Tennis", color: "", textColor:"", icon: ""}];
